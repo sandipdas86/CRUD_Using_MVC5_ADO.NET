90f2c41c3efdba7ef9eb3fcad98b5fa1be2ff817
sandipdas86 <sdas87@gmail.com>
2017-10-21 23:54:50 +0530

    MVC 5 using ADO.NET

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo.Models;
+using MvcDemo.Repository;
+
+namespace MvcDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee/GetAllEmpDetails    
+        public ActionResult GetAllEmpDetails()
+        {
+
+            EmpRepository EmpRepo = new EmpRepository();
+            ModelState.Clear();
+            return View(EmpRepo.GetAllEmployees());
+        }
+        // GET: Employee/AddEmployee    
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+        // POST: Employee/AddEmployee    
+        [HttpPost]
+        public ActionResult AddEmployee(EmpModel Emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    EmpRepository EmpRepo = new EmpRepository();
+
+                    if (EmpRepo.AddEmployee(Emp))
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Employee/EditEmpDetails/5    
+        public ActionResult EditEmpDetails(int id)
+        {
+            EmpRepository EmpRepo = new EmpRepository();
+            return View(EmpRepo.GetAllEmployees().Find(Emp => Emp.EmpId == id));
+        }
+        // POST: Employee/EditEmpDetails/5    
+        [HttpPost]
+
+        public ActionResult EditEmpDetails(int id, EmpModel obj)
+        {
+            try
+            {
+                EmpRepository EmpRepo = new EmpRepository();
+                EmpRepo.UpdateEmployee(obj);
+                return RedirectToAction("GetAllEmpDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Employee/DeleteEmp/5    
+        public ActionResult DeleteEmp(int id)
+        {
+            try
+            {
+                EmpRepository EmpRepo = new EmpRepository();
+                if (EmpRepo.DeleteEmployee(id))
+                {
+                    ViewBag.AlertMsg = "Employee details deleted successfully";
+
+                }
+                return RedirectToAction("GetAllEmpDetails");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }    
+
+        ////
+        //// GET: /Employee/
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// GET: /Employee/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //
+        // GET: /Employee/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Employee/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /Employee/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Employee/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /Employee/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Employee/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ Repository/EmpRepository.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using MvcDemo.Models;
+
+namespace MvcDemo.Repository
+{
+    public class EmpRepository
+    {
+        private SqlConnection con;
+        //To Handle connection related activities    
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["EmpDB"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+        //To Add Employee details    
+        public bool AddEmployee(EmpModel obj)
+        {
+            connection();
+            SqlCommand com = new SqlCommand("AddNewEmpDetails_SP", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Name", obj.Name);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Address);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //To view employee details with generic list     
+        public List<EmpModel> GetAllEmployees()
+        {
+            connection();
+            List<EmpModel> EmpList = new List<EmpModel>();
+
+            SqlCommand com = new SqlCommand("GetEmployees_SP", con);
+            com.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+            //Bind EmpModel generic list using dataRow     
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmpList.Add(
+
+                    new EmpModel
+                    {
+
+                        EmpId = Convert.ToInt32(dr["Id"]),
+                        Name = Convert.ToString(dr["Name"]),
+                        City = Convert.ToString(dr["City"]),
+                        Address = Convert.ToString(dr["Address"])
+
+                    }
+                    );
+            }
+
+            return EmpList;
+        }
+        //To Update Employee details   
+        public bool UpdateEmployee(EmpModel obj)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("UpdateEmpDetails_SP", con);
+
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmpId", obj.EmpId);
+            com.Parameters.AddWithValue("@Name", obj.Name);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Address);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //To delete Employee details  
+        public bool DeleteEmployee(int Id)
+        {
+            connection();
+            SqlCommand com = new SqlCommand("DeleteEmpById_SP", con);
+
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmpId", Id);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }    
+    }
+}
